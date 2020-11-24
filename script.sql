06e91f9c10bf90841f7cd937d30e57513b52a2f2
Harena01 <53835196+Harena01@users.noreply.github.com>
2020-11-24 15:24:14 +0300

    todo , script

---
+++ script.sql
@@ -0,0 +1,46 @@
+create database foot;
+
+use foot;
+
+create table actions (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    types VARCHAR(10) not NULL,
+    chrono INT NOT NULL
+);
+
+create table but (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    equipe INT ,
+    idActions INT NOT NULL,
+    valeur INT,
+    FOREIGN KEY (idActions) REFERENCES actions(id)
+);
+
+create table faute (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    equipe INT ,
+    typeCarton INT ,
+    idActions INT NOT NULL,
+    valeur INT,
+    FOREIGN KEY (idActions) REFERENCES actions(id)
+);
+
+create table possession (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    equipe INT , 
+    valeur FLOAT,
+    idActions INT NOT NULL,
+    FOREIGN KEY (idActions) REFERENCES actions(id)
+);
+
+create table tir (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    equipe INT,
+    typeTir INT , 
+    idActions INT NOT NULL,
+    valeur INT,
+    FOREIGN KEY (idActions) REFERENCES actions(id)
+);
+
+
+insert into 
